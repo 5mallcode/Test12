60e167982a4f7b027baf335ae0f2cd8a9b90161f
rannarith <rannarith@gmail.com>
2018-11-22 02:37:09 +0700

    Convert HTML form to Blade form for Dashboard and Teachers

---
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.admin')
+
+@section('title')
+    <title>Admin Study Lap </title>
+@stop
+
+@section('style')
+
+@stop
+
+
+@section('content-body')
+<section id="dashboard">
+    <h1>Hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iste quibusdam eligendi praesentium molestias, quas deserunt sunt magni. Sint eaque nostrum explicabo laudantium quod dolorem aspernatur dolore officia blanditiis delectus!</p>
+</section>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Dashboard
+Route::get('/admin', 'DashboardController@index');
+
+// Teachers 
+Route::get('/teacher/view-all', 'TeacherController@index');
+Route::get('/teacher/create' , 'TeacherController@create');
+Route::get('/teacher/edit' , 'TeacherController@edit');
+Route::get('/teacher/profile' , 'TeacherController@profile');
